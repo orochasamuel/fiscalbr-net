960de3358266f3e9c509e93c58e62edcf1e96bb0
osamueloliveira <samuelro.net@gmail.com>
2020-09-02 17:39:11 -0300

    [Parte 3] Migração para net standard

---
+++ FiscalBr/FiscalBr.Common/Sped/SpedRegistrosAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiscalBr.Common.Sped
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SpedRegistrosAttribute : Attribute
+    {
+        public SpedRegistrosAttribute(string obrigatoriedadeInicial, string obrigatoriedadeFinal)
+        {
+            ObrigatoriedadeInicialAtt = obrigatoriedadeInicial;
+            ObrigatoriedadeFinalAtt = obrigatoriedadeFinal;
+        }
+
+        protected string ObrigatoriedadeInicialAtt;
+
+        public string ObrigatoriedadeInicial
+        {
+            get
+            {
+                return ObrigatoriedadeInicialAtt;
+            }
+        }
+
+        protected string ObrigatoriedadeFinalAtt;
+
+        public string ObrigatoriedadeFinal
+        {
+            get
+            {
+                return ObrigatoriedadeFinalAtt;
+            }
+        }
+    }
+}
+
