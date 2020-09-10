67fed92d366cc6c04564c4051bbede3f9ad36254
osamueloliveira <samuelro.net@gmail.com>
2020-09-10 17:09:52 -0300

    Inclusão de teste de geração do Registro000 da EFD Fiscal

---
+++ src/FiscalBr.Tests/Sped/Registro0000.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace FiscalBr.Tests.Sped
+{
+    public class Registro0000
+    {
+        [Fact]
+        public void EFDFiscal()
+        {
+            var initialDate = DateTime.Now.AddDays(-(DateTime.Now.Day - 1));
+            var finalDate = initialDate.AddMonths(1).AddDays(-1);
+
+            var expectedResult = 
+                $"|0000|006|0|{initialDate.ToShortDateString().Replace("/", "")}|{finalDate.ToShortDateString().Replace("/", "")}|BANCO DO BRASIL S.A.|00000000000191||GO|123456789|5204508|||A|1|{Environment.NewLine}";
+
+            var currentResult = new EFDFiscal.Bloco0.Registro0000();
+
+            currentResult.CodVer = 6;
+            currentResult.CodFin = Common.IndCodFinalidadeArquivo.RemessaArquivoOriginal;
+            currentResult.DtIni = initialDate;
+            currentResult.DtFin = finalDate;
+            currentResult.Nome = "BANCO DO BRASIL S.A.";
+            currentResult.Cnpj = "00000000000191";
+            currentResult.Uf = "GO";
+            currentResult.Ie = "123456789";
+            currentResult.CodMun = "5204508";
+            currentResult.IndPerfil = Common.IndPerfilArquivo.A;
+            currentResult.IndAtiv = Common.IndTipoAtividade.Outros;
+
+            var currentResultString = Common.Sped.EscreverCamposSped.EscreverCampos(currentResult, false);
+
+            Assert.Equal(expectedResult, currentResultString);
+        }
+    }
+}
