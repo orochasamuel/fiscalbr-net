6aebc0e965dc9c15becbdc1673cbee38aae1213c
osamueloliveira <samuelro.net@gmail.com>
2020-11-17 16:57:37 -0300

    Commit inicial da leitura de arquivos, remoção do arquivo license.txt e remoção do framework net40

---
+++ src/FiscalBr.Common/Sped/LerCamposSped.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FiscalBr.Common.Sped
+{
+    public static class LerCamposSped
+    {
+        public static object LerCampos(this string line, string file = "EFDFiscal")
+        {
+            line = line.Substring(1);
+
+            line = line.Remove(line.LastIndexOf('|'));
+
+            var splittedLine = line.Split('|');
+            
+            // Retorna o registro sem os 'pipes'. Ex: |0000| -> 0000
+            var registro = line.Substring(0, line.IndexOf('|', 1));
+
+            var bloco = registro.Substring(0, 1);
+
+            var toInstantiate = $"FiscalBr.{file}.Bloco{bloco}+Registro{registro}, FiscalBr.{file}";
+
+            var objectType = Type.GetType(toInstantiate);
+
+            var instantiatedObject = Activator.CreateInstance(objectType);
+
+            var properties = ObtemListaComPropriedadesOrdenadas(objectType);
+
+            for (int i = 0; i < properties.Count; i++)
+            {
+                var prop = properties[i];
+
+                bool conversionResult = true;
+
+                object value = splittedLine[i];
+
+                if (prop.CanWrite)
+                {
+                    var propType = prop.PropertyType;
+
+                    if (propType == typeof(Int32))
+                    {
+                        int convertedInt32Value;
+                        conversionResult = Int32.TryParse(value.ToStringSafe(), out convertedInt32Value);
+                        prop.SetValue(instantiatedObject, convertedInt32Value);
+                    }
+                    else if (propType == typeof(DateTime))
+                    {
+                        var dateToConvert = value.ToStringSafe().Insert(2, "/").Insert(5, "/");
+                        DateTime convertedDateTimeValue;
+                        conversionResult = DateTime.TryParse(dateToConvert, out convertedDateTimeValue);
+                        prop.SetValue(instantiatedObject, convertedDateTimeValue);
+                    }
+                    else if (propType.IsEnum)
+                    {
+                        var convertedEnum = Enum.Parse(propType, value.ToStringSafe());
+
+                        prop.SetValue(instantiatedObject, convertedEnum);
+                    }
+                    else
+                    {
+                        if (string.IsNullOrEmpty(value.ToStringSafe()))
+                            prop.SetValue(instantiatedObject, null, null);
+                        else
+                            prop.SetValue(instantiatedObject, value, null);
+                    }
+                }
+            }
+
+            return instantiatedObject;
+        }
+
+        private static List<System.Reflection.PropertyInfo> ObtemListaComPropriedadesOrdenadas(Type tipo)
+        {
+            /*
+             * http://stackoverflow.com/questions/22306689/get-properties-of-class-by-order-using-reflection
+             */
+            return tipo.GetProperties().OrderBy(p => p.GetCustomAttributes(typeof(SpedCamposAttribute), true)
+                .Cast<SpedCamposAttribute>()
+                .Select(a => a.Ordem)
+                .FirstOrDefault())
+                .ToList();
+        }
+    }
+}
--- src/FiscalBr.Tests/Sped/Registro0000.cs
+++ src/FiscalBr.Tests/Sped/Registro0000.cs
@@ -7,7 +7,7 @@ namespace FiscalBr.Tests.Sped
     public class Registro0000
     {
         [Fact]
-        public void EFDFiscal()
+        public void Escrever_Registro_0000_EFDFiscal()
         {
             var initialDate = (DateTime.Now.AddDays(-(DateTime.Now.Day - 1))).Date;
             var finalDate = initialDate.AddMonths(1).AddDays(-1);
@@ -15,26 +15,52 @@ namespace FiscalBr.Tests.Sped
             var formatedInitialDate = initialDate.ToString(new CultureInfo("pt-BR")).Replace("/", "").Split(" ")[0];
             var formatedFinalDate = finalDate.ToString(new CultureInfo("pt-BR")).Replace("/", "").Split(" ")[0];
 
-            var expectedResult = 
+            var expectedResult =
                 $"|0000|006|0|{formatedInitialDate}|{formatedFinalDate}|BANCO DO BRASIL S.A.|00000000000191||GO|123456789|5204508|||A|1|{Environment.NewLine}";
 
-            var currentResult = new EFDFiscal.Bloco0.Registro0000();
+            var source = new EFDFiscal.Bloco0.Registro0000
+            {
+                CodVer = 6,
+                CodFin = Common.IndCodFinalidadeArquivo.RemessaArquivoOriginal,
+                DtIni = initialDate,
+                DtFin = finalDate,
+                Nome = "BANCO DO BRASIL S.A.",
+                Cnpj = "00000000000191",
+                Uf = "GO",
+                Ie = "123456789",
+                CodMun = "5204508",
+                IndPerfil = Common.IndPerfilArquivo.A,
+                IndAtiv = Common.IndTipoAtividade.Outros
+            };
 
-            currentResult.CodVer = 6;
-            currentResult.CodFin = Common.IndCodFinalidadeArquivo.RemessaArquivoOriginal;
-            currentResult.DtIni = initialDate;
-            currentResult.DtFin = finalDate;
-            currentResult.Nome = "BANCO DO BRASIL S.A.";
-            currentResult.Cnpj = "00000000000191";
-            currentResult.Uf = "GO";
-            currentResult.Ie = "123456789";
-            currentResult.CodMun = "5204508";
-            currentResult.IndPerfil = Common.IndPerfilArquivo.A;
-            currentResult.IndAtiv = Common.IndTipoAtividade.Outros;
+            var currentResult = Common.Sped.EscreverCamposSped.EscreverCampos(source, false);
 
-            var currentResultString = Common.Sped.EscreverCamposSped.EscreverCampos(currentResult, false);
+            Assert.Equal(expectedResult, currentResult);
+        }
 
-            Assert.Equal(expectedResult, currentResultString);
+        [Fact]
+        public void Ler_Registro_0000_EFDFiscal()
+        {
+            var source = @"|0000|006|0|01112020|30112020|BANCO DO BRASIL S.A.|00000000000191||GO|123456789|5204508|||A|1|";
+
+            var expectedResult = new EFDFiscal.Bloco0.Registro0000
+            {
+                CodVer = 6,
+                CodFin = Common.IndCodFinalidadeArquivo.RemessaArquivoOriginal,
+                DtIni = new DateTime(2020, 11, 1),
+                DtFin = new DateTime(2020, 11, 30),
+                Nome = "BANCO DO BRASIL S.A.",
+                Cnpj = "00000000000191",
+                Uf = "GO",
+                Ie = "123456789",
+                CodMun = "5204508",
+                IndPerfil = Common.IndPerfilArquivo.A,
+                IndAtiv = Common.IndTipoAtividade.Outros
+            };
+
+            var currentResult = Common.Sped.LerCamposSped.LerCampos(source);
+
+            Assert.Equal(expectedResult, currentResult);
         }
     }
 }
