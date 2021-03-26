0f2ef0148019dac90f305174c01e4b42e237beca
Marcos Gerene Felix Spirito <mgf.spirito@gmail.com>
2021-03-26 09:17:56 -0300

    [+] Continuando processo de leitura, Bloco 0 do Sped Fiscal inteiro já está sendo lido

---
+++ src/FiscalBr.Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using FiscalBr.EFDFiscal;
+using System;
+
+namespace FiscalBr.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sped = new ArquivoEFDFiscal();
+            sped.AoLerLinha += Sped_AoLerLinha;
+            sped.Ler(@"c:\temp\sped.txt");
+
+            Console.Read();
+            
+        }
+
+        private static void Sped_AoLerLinha(object sender, Common.Sped.SpedEventArgs e)
+        {
+            Console.WriteLine(e.Linha);
+        }
+
+    }
+}
