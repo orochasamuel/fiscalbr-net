d5d0b47265d4d3324d1d3355ab13f35be998cf22
Marcos Gerene F. Spirito <mgf.spirito@gmail.com>
2021-04-06 10:53:01 -0300

    [*] Melhora na geração do Bloco 9

--- src/FiscalBr.Common/Sped/ArquivoSped.cs
+++ src/FiscalBr.Common/Sped/ArquivoSped.cs
@@ -8,7 +8,7 @@ namespace FiscalBr.Common.Sped
 {
     public sealed class SpedEventArgs : EventArgs
     {
-        public string Linha { get; set; }        
+        public string Linha { get; set; }
         public RegistroBaseSped Registro { get; set; }
     }
 
@@ -26,32 +26,51 @@ namespace FiscalBr.Common.Sped
 
         public ArquivoSped()
         {
+            Linhas = new List<string>();
+            Erros = new List<string>();
         }
 
         public virtual void Ler(string path, Encoding encoding = null)
         {
             Erros = new List<string>();
 
-            Linhas = File.ReadAllLines(path, encoding).ToList();
+            Linhas = File.ReadAllLines(path, encoding ?? Encoding.Default).ToList();
 
             //Remove linhas em branco
             Linhas.RemoveAll(l => string.IsNullOrEmpty(l.Trim()));
         }
 
-        public virtual void CalcularBloco9()
+        /// <summary>
+        /// Calcula o Bloco 9 - 
+        /// É necessário que as linhas já estejam geradas. Gere manualmente ou utilize a função "GerarLinhas()".
+        /// </summary>
+        /// <param name="totalizarblocos">Calcula o totalizados de todos os blocos, ex: 0990, C990, ...</param>
+        public virtual void CalcularBloco9(bool totalizarblocos = true)
         {
             if (Linhas == null || !Linhas.Any())
-                throw new Exception("Não é possível calcular o bloco 9 sem as linhas. Chame a função \"GerarLinhas()\" ou leia um arquivo pra fazer o calculo");
+                throw new Exception("Não é possível calcular o bloco 9 sem as linhas. Execute a função \"GerarLinhas()\", gere as linhas manualemnte ou leia um arquivo para preencher as linhas.");
         }
 
+        /// <summary>
+        /// Serealiza o objeto no atributo "Linhas"
+        /// </summary>
         public virtual void GerarLinhas()
         {
             Linhas = new List<string>();
         }
 
-        public void Escrever(string path, Encoding encoding = null) =>
-            File.WriteAllLines(path, Linhas.ToArray(), encoding ?? Encoding.Default);
+        public void Escrever(string path, Encoding encoding = null)
+        {
+            if (Linhas == null || !Linhas.Any())
+                throw new Exception("Não é possível escrever sem as linhas. Execute a função \"GerarLinhas()\", gere as linhas manualemnte ou leia um arquivo para preencher as linhas.");
 
+            File.WriteAllLines(path, Linhas.ToArray(), encoding ?? Encoding.Default);
+        }
+
+        /// <summary>
+        /// Adicionar um objeto do tipo RegistroBaseSped no atributo "Linhas".
+        /// Caso existe alguma falha na geração, adiciona a falha no atributo "Erros"
+        /// </summary>
         protected void EscreverLinha(RegistroBaseSped registro)
         {
             string erro;
@@ -61,7 +80,7 @@ namespace FiscalBr.Common.Sped
                 Erros.Add(erro);
 
             Linhas.Add(texto);
-        }      
+        }
     }
 
 }
