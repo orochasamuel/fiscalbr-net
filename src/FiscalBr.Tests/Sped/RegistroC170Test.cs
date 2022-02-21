68aafa29641dd41d4a6ed52014dfef6070089fe4
orochasamuel <samuelro.net@gmail.com>
2022-02-21 14:26:04 -0300

    refactor: remoção de "?" no enum de indicador de apuração IPI do Registro C170 da EFD Fiscal #ISSUE37

---
+++ src/FiscalBr.Tests/Sped/RegistroC170Test.cs
@@ -0,0 +1,34 @@
+﻿using FiscalBr.Common;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xunit;
+
+namespace FiscalBr.Tests.Sped
+{
+    public class RegistroC170Test
+    {
+        [Fact]
+        public void Escrever_Registro_C170_EFDFiscal_V15()
+        {
+            var expectedResult =
+               $"|C170||||||||||||||||||0||||||||||||||||||||{Environment.NewLine}";
+
+            var source = new EFDFiscal.BlocoC.RegistroC170
+            {
+                IndApur = Common.IndPeriodoApuracaoIpi.Mensal
+            };
+
+            var currentResult = Common.Sped.EscreverCamposSped.EscreverCampos(
+                source,
+                CodigoVersaoLeiaute.V15,
+                new DateTime(2015, 01, 01)
+                );
+
+            Assert.Equal(expectedResult, currentResult);
+        }
+
+       
+    }
+}
