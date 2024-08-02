c2dc2ca75d73e14df02cb26e2938e98dcd10d775
fabricio.vieira <supersoftdev01@outlook.com>
2024-08-02 10:32:35 -0300

    Test: Criacao de testes para Bloco D, registros D300 e D310

---
+++ tests/FiscalBr.Test/Sped/BlocoD/BlocoDRegistro310Test.cs
@@ -0,0 +1,31 @@
+﻿using FiscalBr.Common.Sped.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiscalBr.Test.Sped.BlocoD
+{
+    public class BlocoDRegistro310Test
+    {
+        [Fact]
+        public void Escrever_Registro_D310_EFDFiscal()
+        {
+            var expectedResult =
+                $"|D310|3550308|0,00|0,00|0,00|{Environment.NewLine}";
+
+            var source = new EFDFiscal.BlocoD.RegistroD310()
+            {
+                CodMunOrig = "3550308",
+                VlServ = 0M,
+                VlBcIcms = 0M,
+                VlIcms = 0M,
+            };
+
+            var currentResult = Common.Sped.EscreverCamposSped.EscreverCampos(source, VersaoLeiauteSped.V2);
+
+            Assert.Equal(expectedResult, currentResult);
+        }
+    }
+}
